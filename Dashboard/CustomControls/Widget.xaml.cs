2339debfebbf89fdc5f94815b64ab178f19dac8f
Jakub Pietras <james.m.pietras@gmail.com>
2019-01-28 20:12:24 +0100

    #3 Reusable user control widget base

--- Dashboard/CustomControls/Widget.xaml.cs
+++ Dashboard/CustomControls/Widget.xaml.cs
@@ -1,18 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+﻿using Windows.UI.Composition;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Markup;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
 // The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
 
@@ -23,14 +12,11 @@ namespace Dashboard.CustomControls
     {
         public Widget()
         {
-            this.InitializeComponent();
-            // this.Children.Add();
+            InitializeComponent();
         }
 
-        public static readonly DependencyProperty ChildrenProperty =
-            DependencyProperty.Register(nameof(Children), typeof(UIElement), typeof(Widget), new PropertyMetadata(typeof(UIElement)));
-        public static readonly DependencyProperty TextProperty =
-            DependencyProperty.Register(nameof(Text), typeof(string), typeof(Widget), new PropertyMetadata(typeof(string)));
+        public static readonly DependencyProperty ChildrenProperty = DependencyProperty.Register(nameof(Children), typeof(UIElement), typeof(Widget), new PropertyMetadata(typeof(UIElement)));
+        public static readonly DependencyProperty TextProperty = DependencyProperty.Register(nameof(Text), typeof(string), typeof(Widget), new PropertyMetadata(typeof(string)));
 
         public UIElement Children
         {
