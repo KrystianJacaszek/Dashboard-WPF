61eabea0cb7174249953affa031549646befb785
Wojciech Krupa <krupawojciech13@gmail.com>
2019-02-06 00:17:25 +0100

    #9 Quotes Widget v.01

---
+++ Dashboard/CustomControls/QuoteWidget/QuotesWidget.xaml.cs
@@ -0,0 +1,25 @@
+﻿using DashboardLib.Models;
+using Windows.UI.Xaml.Controls;
+
+// Szablon elementu Kontrolka użytkownika jest udokumentowany na stronie https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace Dashboard.CustomControls
+{
+    public sealed partial class QuotesWidget : UserControl
+    {
+        public QuotesWidget()
+        {
+            QuotesModel quotesModel = new QuotesModel();
+            InitializeComponent();
+            SetupQuote();
+        }
+        private void SetupQuote()
+        {
+            Category.Text = "Inspire";
+            Title.Text = "Title of Quote";
+            Date.Text = "Date of Quote";
+            Author.Text = "Author of Quote";
+            Quote.Text = "Quote";
+        }
+    }
+}
