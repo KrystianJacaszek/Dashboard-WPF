cfd572f7e6d96a2d74905f87dafd6fa07e936608
Wojciech Krupa <krupawojciech13@gmail.com>
2019-02-06 01:12:59 +0100

    Usuniecie bledow

---
+++ Dashboard/CustomControls/NewsWidget/NewsWidget.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media.Imaging;
+using DashboardLib.Models;
+
+// Szablon elementu Kontrolka użytkownika jest udokumentowany na stronie https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace Dashboard.CustomControls
+{
+    public sealed partial class NewsWidget : UserControl
+    {
+        NewsModel newsModel = new NewsModel();
+
+        public NewsWidget()
+        {
+            newsModel.SetupList();
+            newsModel.CallAPI();
+
+            InitializeComponent();
+
+        }
+        void Load_News(object sender, RoutedEventArgs e)
+        {
+            SetupNews();
+            LoadNews.Visibility = Visibility.Collapsed;
+        }
+        void Back_Click(object sender, RoutedEventArgs e)
+        {
+            if (newsModel.CurrentNewsPage > 1)
+            {
+                this.newsModel.CurrentNewsPage--;
+                SetupNews();
+            }
+        }
+        void Next_Click(object sender, RoutedEventArgs e)
+        {
+            if (newsModel.CurrentNewsPage < 15)
+            {
+                this.newsModel.CurrentNewsPage++;
+                SetupNews();
+            }
+        }
+        void Next_News(object sender, RoutedEventArgs e)
+        {
+            if (newsModel.CurrentNewsSource < newsModel.URL_Collection.Count - 1)
+            {
+                this.newsModel.CurrentNewsSource++;
+                newsModel.CallAPI();
+                SetupNews();
+            }
+        }
+        void Back_News(object sender, RoutedEventArgs e)
+        {
+            if (newsModel.CurrentNewsSource > 1)
+            {
+                this.newsModel.CurrentNewsSource--;
+                newsModel.CallAPI();
+                SetupNews();
+            }
+        }
+        private void SetupNews()
+        {
+            try
+            {
+                BitmapImage bi = new BitmapImage();
+                bi.CreateOptions = BitmapCreateOptions.None;
+                bi.UriSource = new Uri(newsModel.Root.articles[newsModel.CurrentNewsPage].urlToImage, UriKind.Absolute);
+
+                Image.Source = bi;
+                Title.Text = newsModel.Root.articles[newsModel.CurrentNewsPage].title;
+                Date.Text = newsModel.Root.articles[newsModel.CurrentNewsPage].publishedAt + Environment.NewLine;
+                Source.Text = newsModel.Root.articles[newsModel.CurrentNewsPage].source.name;
+                Counter.Text = $"News Article: {newsModel.CurrentNewsPage}{Environment.NewLine}";
+                NewsText.Text = newsModel.Root.articles[newsModel.CurrentNewsPage].description;
+            }
+            catch (Exception)
+            {
+                NewsText.Text = "Something went wrong try to refresh.";
+            }
+        }
+    }
+}
---
+++ DashboardLib/Models/NewsModel.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace DashboardLib.Models
+{
+    //NEWS CLASSES
+    public class News
+    {
+        public class Rootobject
+        {
+            public string status { get; set; }
+            public int totalResults { get; set; }
+            public Article[] articles { get; set; }
+        }
+
+        public class Article
+        {
+            public Source source { get; set; }
+            public string author { get; set; }
+            public string title { get; set; }
+            public string description { get; set; }
+            public string url { get; set; }
+            public string urlToImage { get; set; }
+            public DateTime publishedAt { get; set; }
+            public string content { get; set; }
+        }
+
+        public class Source
+        {
+            public string id { get; set; }
+            public string name { get; set; }
+        }
+    }
+    public class Http
+    {
+        public static async Task<string> GetVsAsync(string uri)
+        {
+            //HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
+            HttpClient client = new HttpClient();
+            string responseValue = string.Empty;
+            HttpResponseMessage response = await client.GetAsync(uri);
+            if (response.IsSuccessStatusCode)
+            {
+                try
+                {
+                    responseValue = await response.Content.ReadAsStringAsync();
+                }
+                catch (Exception exception)
+                {
+                    Debug.WriteLine(exception.ToString());
+                }
+            }
+            return responseValue;
+            /*
+            using (HttpWebResponse response = (HttpWebResponse)await request.GetResponseAsync())
+            using (Stream stream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                return await reader.ReadToEndAsync();
+            }*/
+        }
+    }
+    public class Config
+    {
+        public static async Task<News.Rootobject> Deserialize(string URI)
+        {
+            var json = await Http.GetVsAsync(URI);
+            return Newtonsoft.Json.JsonConvert.DeserializeObject<News.Rootobject>(json);
+        }
+    }
+    //NEWS CLASSES END
+
+    public class NewsModel
+    {
+        const string PL_SPORT = "https://newsapi.org/v2/top-headlines?country=pl&category=sports&apiKey=3d9608e2e5044857984732bfb6c0e0b2";
+        const string USA_ENTERTAIN_NEWS = "https://newsapi.org/v2/top-headlines?country=us&category=entertainment&apiKey=3d9608e2e5044857984732bfb6c0e0b2";
+        const string USA_SPORT_NEWS = "https://newsapi.org/v2/top-headlines?country=us&category=sports&apiKey=3d9608e2e5044857984732bfb6c0e0b2";
+        const string PL_TECH_NEWS = "https://newsapi.org/v2/top-headlines?sources=google-news&apiKey=3d9608e2e5044857984732bfb6c0e0b2";
+        const string PL_BISS_NEWS = "https://newsapi.org/v2/top-headlines?country=pl&category=business&apiKey=3d9608e2e5044857984732bfb6c0e0b2";
+        const string PL_ENTERTAIN_NEWS = "https://newsapi.org/v2/top-headlines?country=pl&category=entertainment&apiKey=3d9608e2e5044857984732bfb6c0e0b2";
+
+        public List<string> URL_Collection;
+
+        public News.Rootobject Root { get; set; }
+        public int CurrentNewsPage { get; set; } = 1;
+        public int CurrentNewsSource { get; set; } = 0;
+
+        public void SetupList()
+        {
+            this.URL_Collection = new List<string>();
+            this.URL_Collection.Add(PL_BISS_NEWS);
+            this.URL_Collection.Add(PL_TECH_NEWS);
+            this.URL_Collection.Add(PL_ENTERTAIN_NEWS);
+            this.URL_Collection.Add(USA_ENTERTAIN_NEWS);
+            this.URL_Collection.Add(USA_SPORT_NEWS);
+            this.URL_Collection.Add(PL_SPORT);
+        }
+        public async void CallAPI()
+        {
+            CurrentNewsPage = 1;
+            Root = await Config.Deserialize(this.URL_Collection[CurrentNewsSource]);
+        }
+    }
+}
