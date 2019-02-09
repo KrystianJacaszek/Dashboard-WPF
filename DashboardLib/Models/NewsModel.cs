a757f448bfeecfea13ffa0acccaa7b554d688e4b
Wojciech Krupa <krupawojciech13@gmail.com>
2019-02-09 20:33:50 +0100

    Poprawa funkcjonalnosci i wygladu

---
+++ DashboardLib/Models/NewsModel.cs
@@ -0,0 +1,69 @@
+﻿using DashboardLib.ApiModules;
+using System;
+using System.ComponentModel;
+
+namespace DashboardLib.Models
+{
+    public class NewsModel : INotifyPropertyChanged
+    {
+        public NewsModel(string title, DateTime date, string newsText, string image, string newsUrl, string newsSource, string content)
+        {
+            this.title = title;
+            this.date = date;
+            this.newsUrl = newsUrl;
+            this.newsText = newsText;
+            this.newsSource = newsSource;
+            this.image = image;
+        }
+
+        private NewsApi newsApi = NewsApi.Instance;
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged(string propertyName) { PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName)); }
+
+        private string title;
+        private DateTime date;
+        private string newsUrl;
+        private string newsSource;
+        private string newsText;
+        private string image;
+        private string content;
+
+        public string Description => newsText != null ? newsText : content;
+
+        public string Title
+        {
+            get { return title; }
+            set { if (value != title) { title = value; NotifyPropertyChanged("Title"); } }
+        }
+
+        public DateTime Date
+        {
+            get { return date; }
+            set { if (value != date) { date = value; NotifyPropertyChanged("Date"); } }
+        }
+
+        public string NewsUrl
+        {
+            get { return newsUrl; }
+            set { if (value != newsUrl) { newsUrl = value; NotifyPropertyChanged("NewsUrl"); } }
+        }
+
+        public string NewsSource
+        {
+            get { return newsSource; }
+            set { if (value != newsSource) { newsSource = value; NotifyPropertyChanged("NewsSource"); } }
+        }
+
+        public string NewsText
+        {
+            get { return newsText; }
+            set { if (value != newsText) { newsText = value; NotifyPropertyChanged("NewsText"); } }
+        }
+
+        public string Image
+        {
+            get { return image; }
+            set { if (value != image) { image = value; NotifyPropertyChanged("Image"); } }
+        }
+    }
+}
---
+++ DashboardLib/ViewModels/NewsWidgetViewModel.cs
@@ -0,0 +1,86 @@
+﻿using DashboardLib.ApiModules;
+using DashboardLib.Models;
+using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DashboardLib.ViewModels
+{
+    public class NewsWidgetViewModel : BaseViewModel
+    {
+        private NewsApi newsApi = NewsApi.Instance;
+
+
+        public void ChangePage(sbyte modifier)
+        {
+            short targetPage = Convert.ToInt16(CurrentPage + modifier);
+            if (targetPage >= 1 && targetPage <= newsList.Count)
+            {
+                CurrentPage += modifier;
+            }
+        }
+
+
+        public override async Task Initialize()
+        {
+            NewsApi.Rootobject newsData = await newsApi.LoadNews("a");
+
+            if (newsData != null)
+            {
+                NewsModel[] newsListArray = newsData.articles.Select(item =>
+                {
+                    NewsModel entry = new NewsModel(item.title, item.publishedAt, item.description, item.urlToImage, item.url, item.source.name, item.content);
+
+                    return entry;
+
+                }).ToArray();
+                NewsList = new ObservableCollection<NewsModel>(newsListArray);
+            }
+        }
+
+        public override Task Destroy()
+        {
+            return Task.CompletedTask;
+        }
+
+        private ObservableCollection<NewsModel> newsList = new ObservableCollection<NewsModel>();
+
+        public ObservableCollection<NewsModel> NewsList
+        {
+            get { return newsList; }
+            set
+            {
+                if (value != newsList)
+                {
+                    if (value != newsList)
+                    {
+                        newsList = value;
+                        NotifyPropertyChanged("NewsList");
+                        NotifyPropertyChanged("CurrentlyDisplayedNews");
+                    }
+                }
+            }
+        }
+
+        private short currentPage = 1;
+        public short CurrentPage
+        {
+            get { return currentPage; }
+            set
+            {
+                if (value != currentPage)
+                {
+                    if (value != currentPage)
+                    {
+                        currentPage = value;
+                        NotifyPropertyChanged("CurrentPage");
+                        NotifyPropertyChanged("CurrentlyDisplayedNews");
+                    }
+                }
+            }
+        }
+
+        public NewsModel CurrentlyDisplayedNews => newsList.Count > 0 ? newsList[currentPage - 1] : null;
+    }
+}
