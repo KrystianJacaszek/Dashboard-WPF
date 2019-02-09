5c6e7db4177835a1b1f04afec2aad67aa44f0c77
KrystianJacaszek <krystian.jacaszek@gmail.com>
2019-02-09 22:31:01 +0100

    Create unit tests for TodoWidget and NotesWidget

---
+++ DashboardLibTests/ViewModelsTests/NotesWidgetTests.cs
@@ -0,0 +1,48 @@
+﻿using DashboardLib.Models;
+using DashboardLib.ViewModels;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+
+namespace DashboardLibTests
+{
+    [TestClass]
+    class NotesWidgetTests
+    {
+        private NotesWidgetViewModel VM;
+
+        [TestInitialize]
+        public async Task Init()
+        {
+
+            VM = new NotesWidgetViewModel();
+            await VM.Initialize();
+
+            Assert.IsInstanceOfType(VM, typeof(NotesWidgetViewModel));
+
+        }
+
+        [TestMethod]
+        public void ClearNotes() {
+
+
+            string testText = "Testing text";
+            VM.Notes.Content = testText;
+
+            Assert.AreEqual(testText, VM.Notes.Content);
+
+            VM.ClearNotes();
+
+            Assert.IsNull(VM.Notes.Content);
+
+        }
+
+        [TestCleanup]
+        public async Task Destroy()
+        {
+
+            await VM.Destroy();
+            VM = null;
+
+        }
+    }
+}
